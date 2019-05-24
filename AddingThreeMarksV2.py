14ed09e2f9c2fb8ff8a4e2ec7e6591f1aa04f1ff
DanielAShurmer <thatblackshadowcat@outlook.com>
2019-05-24 09:59:08 +0100

    Second Commit Day 1

---
+++ AddingThreeMarksV2.py
@@ -0,0 +1,12 @@
+Physics = 26
+Chemistry = 28
+Maths = 36
+Total = Physics + Chemistry + Maths
+Percent = Total * 100/150
+print("Physics: ", Physics)
+print("Chemistry: ", Chemistry)
+print("Maths: ", Maths)
+print("---------------------------")
+print("Total Marks: ", Total)
+print("Percentage: ", Percent)
+print("---------------------------")
