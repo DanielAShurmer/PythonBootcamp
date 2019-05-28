43711b508bc282503d77ad5b5f0b0c16bf47bbf1
DanielAShurmer <thatblackshadowcat@outlook.com>
2019-05-28 10:32:56 +0100

    First Commit For Day 2

---
+++ BasicWhileLoop.py
@@ -0,0 +1,5 @@
+a=1
+while a <=10:
+    print("Hello")
+    a += 1
+
---
+++ TimesTableGenerator.py
@@ -0,0 +1,5 @@
+TimesTable = int(input("Enter a Number:"))
+Count = 1
+while Count <= 10:
+    print(TimesTable,"X",Count,"=",(TimesTable * Count))
+    Count += 1
